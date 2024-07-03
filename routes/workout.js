4d4ba1fa90b04e9c88527d61af951203715b7d31
acharya <sav.acharya22@gmail.com>
2024-07-03 11:41:06 +1000

    add workout model to db

--- routes/workout.js
+++ routes/workout.js
@@ -1,33 +1,40 @@
+const express = require("express");
+const Workout = require("../models/workoutModel");
 
-
-const express = require('express')
-
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.json({ message: "GEt all workouts route!" })
-})
-
+  res.json({ message: "GEt all workouts route!" });
+});
 
 // Get a single workout
 router.get("/:id", (req, res) => {
-    res.json({ message: `GET workout by id ${req.params.id}!` })
-})
+  res.json({ message: `GET workout by id ${req.params.id}!` });
+});
 
 // POST a new workout
-router.post("/", (req, res) => {
-    res.json({ message: "POST new workout route!" })
-})
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({
+      title,
+      load,
+      reps,
+    });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // PAtch update a workout
 router.patch("/:id", (req, res) => {
-    res.json({ message: `PATCH workout by id ${req.params.id}!` })
-})
+  res.json({ message: `PATCH workout by id ${req.params.id}!` });
+});
 
 // DELETE a workout
 router.delete("/:id", (req, res) => {
-    res.json({ message: `DELETE workout by id ${req.params.id}!` })
-})
+  res.json({ message: `DELETE workout by id ${req.params.id}!` });
+});
 
-
-module.exports = router
+module.exports = router;
