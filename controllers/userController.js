49259156d7f76bc98689923c46c15dd48da52d07
acharya <sav.acharya22@gmail.com>
2024-07-09 19:59:11 +1000

    set up user sign up and hashing password of new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,7 +15,16 @@ const loginUser = async (req, res) => {
 
 const signupUser = async (req, res) => {
 
-    res.json({message :  'signup user'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.signup(email, password)
+
+        res.status(200).json({email, user})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
+
 }
 
 module.exports = {
--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,8 @@
 
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcryptjs');
+
 const Schema = mongoose.Schema
 
 const userSchema = new Schema ({
@@ -16,5 +18,20 @@ const userSchema = new Schema ({
         minlength: 8
     }
 })
+
+// static signup method
+userSchema.statics.signup = async function (email, password)  {
+
+        const exists = await this.findOne({email})
+        if (exists) {
+            throw new Error('Email already exists')
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await this.create({email, password: hashedPassword})
+
+        return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
