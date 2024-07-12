0c660eb575059472ef76516aaf4b406347c628fb
acharya <sav.acharya22@gmail.com>
2024-07-12 21:12:05 +1000

    create jwt token while sign up

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,13 @@
 
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+
+const createToken = (_id) => {
+    
+  return  jwt.sign({_id}, process.env.SECRET,  {expiresIn: '3d'} )
+
+}
 
 // login user
 
@@ -20,7 +28,11 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        // create token
+
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     } catch (err) {
         res.status(400).json({error: err.message})
     }
