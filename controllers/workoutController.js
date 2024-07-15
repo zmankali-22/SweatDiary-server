d53b94f3ad84a7c0b7977214dac6ff8aed9a829c
acharya <sav.acharya22@gmail.com>
2024-07-15 20:45:25 +1000

    assign the workouts to users

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 
 const getWorkouts = async (req, res) => {
 
-    const workouts = await Workout.find({}).sort({createdAt: -1})
+    const user_id = req.user._id
+    const workouts = await Workout.find({user_id}).sort({createdAt: -1})
     res.status(200).json(workouts)
 }
 
@@ -52,10 +53,12 @@ const createWorkout =  async (req, res) => {
 
     // add doc to db
     try {
+        const user_id = req.user._id
       const workout = await Workout.create({
         title,
         load,
         reps,
+        user_id,
       });
       res.status(200).json(workout);
     } catch (error) {
